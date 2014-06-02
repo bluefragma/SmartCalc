692d0bf53f78656101a0d83d8de04debf3a8a6b5
bluefragma <dkyang73@gmail.com>
2014-06-03 00:42:20 +0900

    시작

--- SmartCalc/ViewController.h
+++ SmartCalc/ViewController.h
@@ -8,6 +8,66 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+typedef enum {
+    STATUS_DEFAULT=0,
+    STATUS_DIVISION,
+    STATUS_MULTIPLY,
+    STATUS_MINUS,
+    STATUS_PLUS,
+    STATUS_RETURN
+}kStatusCode;
+
+@interface ViewController : UIViewController {
+    double curValue;
+    double totalCurValue;
+    NSString *curInputValue;
+    
+    IBOutlet UILabel *displayLabel;
+    IBOutlet UIButton *keypadMC;
+    IBOutlet UIButton *keypadMPlus;
+    IBOutlet UIButton *keypadMMinus;
+    IBOutlet UIButton *keypadMR;
+    IBOutlet UIButton *keypadC;
+    IBOutlet UIButton *keypadPM;
+    IBOutlet UIButton *keypadPercent;
+    IBOutlet UIButton *keypadDivide;
+    IBOutlet UIButton *keypadMultiple;
+    IBOutlet UIButton *keypadMinus;
+    IBOutlet UIButton *keypadPlus;
+    IBOutlet UIButton *keypadEqual;
+    IBOutlet UIButton *keypad0;
+    IBOutlet UIButton *keypad1;
+    IBOutlet UIButton *keypad2;
+    IBOutlet UIButton *keypad3;
+    IBOutlet UIButton *keypad4;
+    IBOutlet UIButton *keypad5;
+    IBOutlet UIButton *keypad6;
+    IBOutlet UIButton *keypad7;
+    IBOutlet UIButton *keypad8;
+    IBOutlet UIButton *keypad9;
+    IBOutlet UIButton *keypadPoint;
+}
+
+-(IBAction) disitPressed:(UIButton *)sender;
+-(IBAction)operationpressed:(UIButton *)sender;
+-(void) Calculation:(kStatusCode)StatusCode CurStatusCode:(kStatusCode)cStatusCode;
+-(void) ClearCalulation;
+-(void) DisplayInputValue:(NSString *)displayText;
+-(void) DisplayCalculationValue;
+
+-(NSString *)ConvertComma:(NSString *)data;
+
+-(void) MinusCalculation;
+-(void) PlusClaculation;
+-(void) MultiplyCalculation;
+-(void) DivisionCalculation;
+-(void) RemainCalculationValue;
+-(void)ReturnCalculationValue;
+
+@property Float64 curValue;
+@property Float64 totalCurValue;
+@property kStatusCode curStatusCode;
+@property (nonatomic, retain) NSString *curInputValue;
+@property (nonatomic, retain) UILabel *displayLabel;
 
 @end
--- SmartCalc/ViewController.m
+++ SmartCalc/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
 
@@ -14,10 +15,104 @@
 
 @implementation ViewController
 
+@synthesize curValue, totalCurValue, curStatusCode, curInputValue, displayLabel;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    displayLabel.layer.cornerRadius = 5;
+    keypadMC.layer.cornerRadius = 5;
+    keypadMPlus.layer.cornerRadius = 5;
+    keypadMMinus.layer.cornerRadius = 5;
+    keypadMR.layer.cornerRadius = 5;
+    keypadC.layer.cornerRadius = 5;
+    keypadPM.layer.cornerRadius = 5;
+    keypadPercent.layer.cornerRadius = 5;
+    keypadDivide.layer.cornerRadius = 5;
+    keypadMultiple.layer.cornerRadius = 5;
+    keypadMinus.layer.cornerRadius = 5;
+    keypadPlus.layer.cornerRadius = 5;
+    keypadEqual.layer.cornerRadius = 5;
+    keypad0.layer.cornerRadius = 5;
+    keypad1.layer.cornerRadius = 5;
+    keypad2.layer.cornerRadius = 5;
+    keypad3.layer.cornerRadius = 5;
+    keypad4.layer.cornerRadius = 5;
+    keypad5.layer.cornerRadius = 5;
+    keypad6.layer.cornerRadius = 5;
+    keypad7.layer.cornerRadius = 5;
+    keypad8.layer.cornerRadius = 5;
+    keypad9.layer.cornerRadius = 5;
+    keypadPoint.layer.cornerRadius = 5;
+}
+
+-(IBAction) disitPressed:(UIButton *)sender
+{
+    
+}
+
+-(IBAction)operationpressed:(UIButton *)sender
+{
+    
+}
+
+-(void) Calculation:(kStatusCode)StatusCode CurStatusCode:(kStatusCode)cStatusCode
+{
+    
+}
+
+-(void) ClearCalulation
+{
+    
+}
+
+-(void) DisplayInputValue:(NSString *)displayText
+{
+    
+}
+
+-(void) DisplayCalculationValue
+{
+    
+}
+
+
+-(NSString *)ConvertComma:(NSString *)data
+{
+    NSString * aa;
+    return aa;
+}
+
+
+-(void) MinusCalculation
+{
+    
+}
+
+-(void) PlusClaculation
+{
+    
+}
+
+-(void) MultiplyCalculation
+{
+    
+}
+
+-(void) DivisionCalculation
+{
+    
+}
+
+-(void) RemainCalculationValue
+{
+    
+}
+
+-(void)ReturnCalculationValue
+{
+    
 }
 
 - (void)didReceiveMemoryWarning
