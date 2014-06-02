55f1c703aea79f9e6c73d5962437612e5bc9ae45
bluefragma <dkyang73@gmail.com>
2014-06-03 00:19:19 +0900

    Initial Commit

---
+++ SmartCalc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SmartCalc
+//
+//  Created by 양동길 on 2014. 6. 3..
+//  Copyright (c) 2014년 양동길. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SmartCalc/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SmartCalc
+//
+//  Created by 양동길 on 2014. 6. 3..
+//  Copyright (c) 2014년 양동길. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
